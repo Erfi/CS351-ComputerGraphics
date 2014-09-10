c83467baa66412687d8c54001b44a78162e48171
Erfi <erfan.basiri.azad@gmail.com>
2014-09-10 14:25:02 -0400

    Done but not cleaned

--- Project_01/src/lab1.c
+++ Project_01/src/lab1.c
@@ -11,57 +11,73 @@
 #define USECPP 0
 
 int main(int argc, char *argv[]) {
-  Pixel *image;
+  Pixel *image1;
+  Pixel *image2;
+  Pixel *imageMask;
   int rows, cols, colors;
   long imagesize;
   long i;
-  int intensity, avg_r, avg_g, avg_b;
-  int green_deviation;
+  //int intensity, avg_r, avg_g, avg_b;
+  //int green_deviation;
+  float alpha;
+  int dx, dy; //offsets
 
-  if(argc < 3) {
-    printf("Usage: ppmtest <input file> <output file>\n");
+  if(argc < 5) {
+    printf("Usage: %s <input file_1><input file_2><input mask><output file>\n", argv[0]);
     exit(-1);
   }
 
   /* read in the image */
-  image = readPPM(&rows, &cols, &colors, argv[1]);
-  if(!image) {
-    fprintf(stderr, "Unable to read %s\n", argv[1]);
+  image1 = readPPM(&rows, &cols, &colors, argv[1]);
+  if(!image1) {
+    fprintf(stderr, "Unable to read file_1 %s\n", argv[1]);
     exit(-1);
+  }else{
+    printf("successfully opened image %s\n", argv[1]);
+  }
+
+  image2 = readPPM(&rows, &cols, &colors, argv[2]);
+  if(!image2) {
+    fprintf(stderr, "Unable to read file_2 %s\n", argv[2]);
+    exit(-1);
+  }else{
+    printf("successfully opened image %s\n", argv[2]);
+  }
+
+  imageMask = readPPM(&rows, &cols, &colors, argv[3]);
+  if(!imageMask) {
+    fprintf(stderr, "Unable to read mask %s\n", argv[3]);
+    exit(-1);
+  }else{
+    printf("successfully opened image %s\n", argv[3]);
   }
 
   /* calculate the image size */
   imagesize = (long)rows * (long)cols;
-
-  /* mess with the image here
-   * In this case it it making a horizontal blur by averaging each 5 pixel */
+  dx = 100;
+  dy = 100;
+  /* mess with the image here */
   for(i=0;i<imagesize;i++) {
-    green_deviation = abs((int)image[i].g - 108);
-    if (((int)image[i].g > 2*(int)image[i].r) && ((int)image[i].g > (int)image[i].b)){
-      image[i].r = 255;
-      image[i].g = 255;
-      image[i].b = 255;
+    int transition = i+dx+dy*cols;
+    if (transition > imagesize){
+      transition = transition - imagesize;
     }
-    else{
-      image[i].r = 0;
-      image[i].g = 0;
-      image[i].b = 0;
-    }
-    // this little piece of code converts the image to inverse greyscale
-    //intensity = ((int)image[i].r + (int)image[i].g + (int)image[i].b) / 3;
-    //image[i].r = 255 - intensity;
-    //image[i].g = 255 - intensity;
-    //image[i].b = 255 - intensity;
+    alpha = imageMask[transition].g/255.0; /*r,g,b of the mask are the same*/
+    image1[i].r = (alpha * image1[i].r) + ((1-alpha) * image2[transition].r);
+    image1[i].g = (alpha * image1[i].g) + ((1-alpha) * image2[transition].g);
+    image1[i].b = (alpha * image1[i].b) + ((1-alpha) * image2[transition].b);
   }
 
-  /* write out the resulting image */
-  writePPM(image, rows, cols, colors /* s/b 255 */, argv[2]);
+//   /* write out the resulting image */
+  writePPM(image1, rows, cols, colors /* s/b 255 */, argv[4]);
 
-  /* free the image memory */
+//   /* free the image memory */
 #if USECPP
   delete[] image;
 #else
-  free(image);
+  free(image1);
+  free(image2);
+  free(imageMask);
 #endif
 
   return(0);
@@ -95,5 +111,18 @@ int main(int argc, char *argv[]) {
     //   image[i].b = avg_b;
     // }
 ..........
+//creates a mask of image. puts 255 on green screen and 0 elsewhere.
+  green_deviation = abs((int)image[i].g - 108);
+    if (((int)image[i].g > 2*(int)image[i].r) && ((int)image[i].g > (int)image[i].b)){
+      image[i].r = 255;
+      image[i].g = 255;
+      image[i].b = 255;
+    }
+    else{
+      image[i].r = 0;
+      image[i].g = 0;
+      image[i].b = 0;
+    }
+
 */
 
