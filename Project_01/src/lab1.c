974ce93c853e67663424e3290ef3810c2e44b0e4
Erfi <erfan.basiri.azad@gmail.com>
2014-09-09 22:42:10 -0400

    task #3

--- Project_01/src/lab1.c
+++ Project_01/src/lab1.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   int rows, cols, colors;
   long imagesize;
   long i;
-  int intensity;
+  int intensity, avg_r, avg_g, avg_b;
 
   if(argc < 3) {
     printf("Usage: ppmtest <input file> <output file>\n");
@@ -31,14 +31,32 @@ int main(int argc, char *argv[]) {
   /* calculate the image size */
   imagesize = (long)rows * (long)cols;
 
-  /* mess with the image here  */
+  /* mess with the image here
+   * In this case it it making a horizontal blur by averaging each 5 pixel */
   for(i=0;i<imagesize;i++) {
+    if (i < 5) {
+      avg_r = ((int)image[0].r + (int)image[1].r + (int)image[2].r + (int)image[3].r + (int)image[4].r)/5;
+      avg_g = ((int)image[0].g + (int)image[1].g + (int)image[2].g + (int)image[3].g + (int)image[4].g)/5;
+      avg_b = ((int)image[0].b + (int)image[1].b + (int)image[2].b + (int)image[3].b + (int)image[4].b)/5;
 
+      image[i].r = avg_r;
+      image[i].g = avg_g;
+      image[i].b = avg_b;
+    }
+    else{
+      avg_r = ((int)image[i-4].r + (int)image[i-3].r + (int)image[i-2].r + (int)image[i-1].r + (int)image[i].r)/5;
+      avg_g = ((int)image[i-4].g + (int)image[i-3].g + (int)image[i-2].g + (int)image[i-1].g + (int)image[i].g)/5;
+      avg_b = ((int)image[i-4].b + (int)image[i-3].b + (int)image[i-2].b + (int)image[i-1].b + (int)image[i].b)/5;
+
+      image[i].r = avg_r;
+      image[i].g = avg_g;
+      image[i].b = avg_b;
+    }
     // this little piece of code converts the image to inverse greyscale
-    intensity = ((int)image[i].r + (int)image[i].g + (int)image[i].b) / 3;
-    image[i].r = 255 - intensity;
-    image[i].g = 255 - intensity;
-    image[i].b = 255 - intensity;
+    //intensity = ((int)image[i].r + (int)image[i].g + (int)image[i].b) / 3;
+    //image[i].r = 255 - intensity;
+    //image[i].g = 255 - intensity;
+    //image[i].b = 255 - intensity;
   }
 
   /* write out the resulting image */
