9ec9a6241b1d086724836ead143d0058432ea63f
Erfi <erfan.basiri.azad@gmail.com>
2014-10-09 20:09:36 -0400

    starting project 5

---
+++ Project_05/include/point.h
@@ -0,0 +1,29 @@
+//
+//  point.h
+//  
+//
+//  Created by Ryan Brandt on 9/22/14.
+//
+//
+
+#ifndef __POINT_H__
+#define __POINT_H__
+
+#include "Image.h"
+#include "color.h"
+
+
+typedef struct{
+    double val[4];
+} Point;
+
+
+
+void point_set2D(Point *p, double x, double y);
+void point_set3D(Point *p, double x, double y, double z);
+void point_set(Point *p, double x, double y, double z, double h);
+void point_copy(Point *to, Point *from);
+void point_draw(Point *p, Image *src, Color c);
+void point_drawf(Point *p, Image *src, FPixel c);
+
+#endif /* defined(__POINT_H__) */
---
+++ Project_05/lib/matrix.c
@@ -0,0 +1,5 @@
+/*
+Erfan Azad
+Date: 9 Oct 2014
+File: matrix.c
+*/
