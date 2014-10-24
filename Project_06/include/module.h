7602edb756bc86bb82354e63c589d95c17fdb269
Erfi <erfan.basiri.azad@gmail.com>
2014-10-24 11:36:30 -0400

    p1

--- Project_06/include/module.h
+++ Project_06/include/module.h
@@ -7,6 +7,13 @@ File: module.h
 #ifndef __MODULE_H__
 #define __MODULE_H__
 
+#include "point.h"
+#include "line.h"
+#include "polyline.h"
+#include "polygon.h"
+#include "color.h"
+#include "matrix.h"
+
 typedef enum { // example of an enumerated type
 	ObjNone,
 	ObjLine,
@@ -44,6 +51,41 @@ typedef struct {
 	Element *head; // pointer to the head of the linked list
 	Element *tail; // keep around a pointer to the last object
 } Module;
+
+
+//functions
+Element *element_create();
+Element *element_init(ObjectType type, void *obj);
+void element_delete(Element *e);
+Module *module_create();
+void module_clear(Module *md);
+void module_delete(Module *md);
+void module_insert(Module *md, Element *e);
+void module_module(Module *md, Module *sub);
+void module_point(Module *md, Point *p);
+void module_line(Module *md, Line *p);
+void module_polyline(Module *md, Polyline *p);
+void module_polygon(Module *md, Polygon *p);
+void module_identity(Module *md);
+void module_translate2D(Module *md, double tx, double ty);
+void module_scale2D(Module *md, double sx, double sy);
+void module_rotateZ(Module *md, double cth, double sth);
+void module_shear2D(Module *md, double shx, double shy);
+void module_draw(Module *md, Matrix *VTM, Matrix *GTM, DrawState *ds, Lighting *lighting, Image *src);
+//3D functions
+void module_translate(Module *md, double tx, double ty, double tz);
+void module_scale(Module *md, double sx, double sy, double sz);
+void module_rotateX(Module *md, double cth, double sth);
+void module_rotateY(Module *md, double cth, double sth);
+void module_rotateXYZ(Module *md, Vector *u, Vector *v, Vector *w);
+void module_cube(Module *md, int solid);
+//shading/color module functions
+void module_color(Module *md, Color *c);
+
+
+
+
+
 
 
 #endif
--- Project_06/lib/module.c
+++ Project_06/lib/module.c
@@ -3,5 +3,92 @@ Ryan Brandt & Erfan Azad
 Date: 24 Oct 2014
 File: module.c
 */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//Allocate and return an initialized but empty Element.
+Element *element_create();
+
+//Allocate an Element and store a duplicate of the data pointed to by obj in the Element. Modules do
+//not get duplicated. The function needs to handle each type of object separately in a case statement.
+Element *element_init(ObjectType type, void *obj);
+
+//free the element and the object it contains, as appropriate.
+void element_delete(Element *e);
+
+//Allocate an empty module
+Module *module_create();
+
+// clear the module’s list of Elements, freeing memory as appropriate.
+void module_clear(Module *md);
+
+// Free all of the memory associated with a module, including the memory pointed to by md
+void module_delete(Module *md);
+
+// Generic insert of an element into the module at the tail of the list.
+void module_insert(Module *md, Element *e);
+
+// Adds a pointer to the Module sub to the tail of the module’s list.
+void module_module(Module *md, Module *sub);
+
+// Adds p to the tail of the module’s list.
+void module_point(Module *md, Point *p);
+
+// Adds p to the tail of the module’s list.
+void module_line(Module *md, Line *p);
+
+// Adds p to the tail of the module’s list.
+void module_polyline(Module *md, Polyline *p);
+
+// Adds p to the tail of the module’s list.
+void module_polygon(Module *md, Polygon *p);
+
+// Object that sets the current transform to the identity, placed at the tail of the module’s list.
+void module_identity(Module *md);
+
+// Matrix operand to add a translation matrix to the tail of the module’s list.
+void module_translate2D(Module *md, double tx, double ty);
+
+// Matrix operand to add a scale matrix to the tail of the module’s list.
+void module_scale2D(Module *md, double sx, double sy);
+
+// Matrix operand to add a rotation about the Z axis to the tail of the module’s list.
+void module_rotateZ(Module *md, double cth, double sth);
+
+// Matrix operand to add a 2D shear matrix to the tail of the module’s list.
+void module_shear2D(Module *md, double shx, double shy);
+
+//Draw the module into the image using the given view transformation matrix [VTM], Lighting and
+//DrawState by traversing the list of Elements. (For now, Lighting can be an empty structure.)
+void module_draw(Module *md, Matrix *VTM, Matrix *GTM, DrawState *ds, Lighting *lighting, Image *src);
+
+
+//3D functions
+
+// Matrix operand to add a 3D translation to the Module.
+void module_translate(Module *md, double tx, double ty, double tz);
+
+// Matrix operand to add a 3D scale to the Module.
+void module_scale(Module *md, double sx, double sy, double sz);
+
+// Matrix operand to add a rotation about the X-axis to the Module.
+void module_rotateX(Module *md, double cth, double sth);
+
+// Matrix operand to add a rotation about the Y-axis to the Module.
+void module_rotateY(Module *md, double cth, double sth);
+
+//Matrix operand to add a rotation that orients to the orthonormal axes ~u, ~v, w~
+void module_rotateXYZ(Module *md, Vector *u, Vector *v, Vector *w);
+
+//Adds a unit cube, axis-aligned and centered on zero to the Module. If solid is zero, add only lines.
+//If solid is non-zero, use polygons. Make sure each polygon has surface normals defined for it.
+void module_cube(Module *md, int solid);
+
+
+//shading/color module functions
+
+// Adds the foreground color value to the tail of the module’s list.
+void module_color(Module *md, Color *c);
 
 
