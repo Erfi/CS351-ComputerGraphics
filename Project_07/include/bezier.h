68e89ade7ab51af720888b7b7fc7a2dd50a9f6d5
Erfi <erfan.basiri.azad@gmail.com>
2014-11-06 19:48:45 -0500

    up

--- Project_07/include/bezier.h
+++ Project_07/include/bezier.h
@@ -7,6 +7,10 @@ File: bezier.h
 #ifndef __BEZIER_H__
 #define __BEZIER_H__
 
+#include "point.h"
+#include "color.h"
+#include "Image.h"
+
 typedef struct{
 	Point ctrl[4];
 	int zBuffer;
@@ -17,16 +21,12 @@ typedef struct{
 	int zBuffer;
 }BezierSurface;
 
-void bezierCurve_init(BezierCurve *b);
-void bezierSurface_init(BezierSurface *b);
-void bezierCurve_set(BezierCurve *b, Point *vlist);
-void bezierSurface_set(BezierSurface *b, Point *vlist);
-void bezierCurve_zBuffer(BezierCurve *p, int flag);
-void bezierSurface_zBuffer(BezierCurve *p, int flag);
-void bezierCurve_draw(BezierCurve *b, Image *src, Color c);
-
-
-
-
+// void bezierCurve_init(BezierCurve *b);
+// void bezierSurface_init(BezierSurface *b);
+// void bezierCurve_set(BezierCurve *b, Point *vlist);
+// void bezierSurface_set(BezierSurface *b, Point *vlist);
+// void bezierCurve_zBuffer(BezierCurve *p, int flag);
+// void bezierSurface_zBuffer(BezierCurve *p, int flag);
+// void bezierCurve_draw(BezierCurve *b, Image *src, Color c);
 
 #endif
--- Project_07/lib/bezier.c
+++ Project_07/lib/bezier.c
@@ -4,60 +4,67 @@ Date: 4 November 2014
 File: bezier.c
 */
 
-//sets the zbuffer flag to 1 and the curve points to the X-axis between 0 and 1.
-void bezierCurve_init(BezierCurve *b){
-	if(NULL != b){
-		b->flag = 1;
-		int i;
-		for(i=0; i<4; i++){
-			b->ctrl[i].val[0] = 0.5;
-		}
-	}
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include "bezier.h"
+#include "point.h"
+#include "color.h"
+#include "Image.h"
 
-//sets the zbuffer flag to 1 and the surface to the X-Z plane between (0, 0) and (1, 1).
-void bezierSurface_init(BezierSurface *b){
-	if(NULL != b){
-		b->flag = 1;
-		int i;
-		for(i=0; i<16; i++){
-			b->ctrl[i].val[0] = 0.5;
-			b->ctrl[i].val[2] = 0.5;
-		}
-	}
-}
+// //sets the zbuffer flag to 1 and the curve points to the X-axis between 0 and 1.
+// void bezierCurve_init(BezierCurve *b){
+// 	if(NULL != b){
+// 		b->flag = 1;
+// 		int i;
+// 		for(i=0; i<4; i++){
+// 			b->ctrl[i].val[0] = 0.5;
+// 		}
+// 	}
+// }
 
-//sets the control points of the BezierCurve to the four points in the vlist array.
-void bezierCurve_set(BezierCurve *b, Point *vlist){
-	if((NULL != b) && (NULL != vlist)){
-		int i;
-		for(i=0; i<4; i++){
-			point_copy(&b->ctrl[i], &vlist[i]);
-		}
-	}
-}
+// //sets the zbuffer flag to 1 and the surface to the X-Z plane between (0, 0) and (1, 1).
+// void bezierSurface_init(BezierSurface *b){
+// 	if(NULL != b){
+// 		b->flag = 1;
+// 		int i;
+// 		for(i=0; i<16; i++){
+// 			b->ctrl[i].val[0] = 0.5;
+// 			b->ctrl[i].val[2] = 0.5;
+// 		}
+// 	}
+// }
 
-//sets the control points of the BezierSurface to the 16 points in the vlist array.
-void bezierSurface_set(BezierSurface *b, Point *vlist){
-	if((NULL != b) && (NULL != vlist)){
-		int i;
-		for(i=0; i<4; i++){
-			point_copy(&b->ctrl[i], &vlist[i]);
-		}
-	}
-}
-void bezierCurve_zBuffer(BezierCurve *p, int flag){
-	if(NULL != p){
-		p->flag = flag;
-	}
-}
+// //sets the control points of the BezierCurve to the four points in the vlist array.
+// void bezierCurve_set(BezierCurve *b, Point *vlist){
+// 	if((NULL != b) && (NULL != vlist)){
+// 		int i;
+// 		for(i=0; i<4; i++){
+// 			point_copy(&b->ctrl[i], &vlist[i]);
+// 		}
+// 	}
+// }
 
-void bezierSurface_zBuffer(BezierCurve *p, int flag){
-	if(NULL != p){
-		p->flag = flag;
-	}
-}
-void bezierCurve_draw(BezierCurve *b, Image *src, Color c){
+// //sets the control points of the BezierSurface to the 16 points in the vlist array.
+// void bezierSurface_set(BezierSurface *b, Point *vlist){
+// 	if((NULL != b) && (NULL != vlist)){
+// 		int i;
+// 		for(i=0; i<4; i++){
+// 			point_copy(&b->ctrl[i], &vlist[i]);
+// 		}
+// 	}
+// }
+// void bezierCurve_zBuffer(BezierCurve *p, int flag){
+// 	if(NULL != p){
+// 		p->flag = flag;
+// 	}
+// }
+
+// void bezierSurface_zBuffer(BezierCurve *p, int flag){
+// 	if(NULL != p){
+// 		p->flag = flag;
+// 	}
+// }
+// void bezierCurve_draw(BezierCurve *b, Image *src, Color c){
 	
-}
+// }
 
